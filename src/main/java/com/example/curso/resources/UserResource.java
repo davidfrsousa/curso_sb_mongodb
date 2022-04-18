f464a761e8d0d8a38f103a6d70f10502b8a4c317
tndg1 <davidfrsousa@hotmail.com>
2022-04-18 16:43:14 -0300

    User DTO

--- src/main/java/com/example/curso/resources/UserResource.java
+++ src/main/java/com/example/curso/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.curso.domain.User;
+import com.example.curso.dto.UserDTO;
 import com.example.curso.services.UserService;
 
 @RestController
@@ -19,8 +20,9 @@ public class UserResource {
 	private UserService userService;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> lista = userService.findAll();
-		return ResponseEntity.ok().body(lista);
+		List<UserDTO> listaDto = lista.stream().map(x -> new UserDTO(x)).toList();
+		return ResponseEntity.ok().body(listaDto);
 	}
 }
