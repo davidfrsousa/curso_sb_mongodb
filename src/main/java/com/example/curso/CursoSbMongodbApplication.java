579d335bcd83d476be0ccf030a4d692747e6c33e
tndg1 <davidfrsousa@hotmail.com>
2022-04-18 15:29:18 -0300

    First commit

---
+++ src/main/java/com/example/curso/CursoSbMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSbMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSbMongodbApplication.class, args);
+	}
+
+}
